c9eb9a595e037a933b4c6606ee1427866e35611e
Jorge Z <mzapataj@uninorte.edu.co>
2018-09-17 09:41:33 -0500

    Cola de prioridades corregida

--- src/javafxapplication2/Reconocimiento.java
+++ src/javafxapplication2/Reconocimiento.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.EmptyStackException;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -16,6 +17,7 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.collections.transformation.SortedList;
 
 /**
  *
@@ -39,7 +41,10 @@ public class Reconocimiento {
     public Reconocimiento(){
         ExpRegular = sc.nextLine();
         String tokenAnterior = "";
-        
+        if (!"(".equals(ExpRegular.substring(0, 1)) && !")".equals(ExpRegular.substring(ExpRegular.length()-1, ExpRegular.length()))) {
+            ExpRegular = "(" + ExpRegular;
+            ExpRegular = ExpRegular + ")";
+        }
         for(String c : ExpRegular.split("")){
             tokens.add(reconocerToken(c,tokenAnterior));
             tokenAnterior = c;
@@ -54,16 +59,15 @@ public class Reconocimiento {
                 colaPrioridadIndex = getIndexOfParentheses();
                 System.out.println("Division de parentesis");
                 System.out.println(colaPrioridadIndex);
-                generateTerms();
+                OrdenOperaciones();
                 System.out.println(terminos);
             }catch(Exception e){
                 e.printStackTrace();
             }
-               
         }
-        
         System.out.println(tokens);
     }
+    
     public String reconocerToken(String token,String tokenAnterior){
         switch(token){
             case "|":
@@ -114,33 +118,46 @@ public class Reconocimiento {
             }
             li++;
         }
-        aux = new LinkedList();
-        aux.add(0);
-        aux.add(tokens.size()-1);
-        result.add(aux);
         return result;
     }
     public void generateTerms(){
         
         List laux = (List)colaPrioridadIndex.get(0);
         terminos.add(tokens.subList((int)laux.get(0)+1, (int)laux.get(1)));
+        List terminosSortedbyLeastIndex = new LinkedList();
+        for (Object object : colaPrioridadIndex) {
+            List ls = (List)object;
+            terminosSortedbyLeastIndex.add(ls);
+        }
+        SortByLeastIndex(terminosSortedbyLeastIndex, colaPrioridadIndex.size());
+        
         //colaPrioridad.add(terminos);
         
         for (int i = 1; i < colaPrioridadIndex.size(); i++) {
             laux = (List)colaPrioridadIndex.get(i);
-            List laux2 = (List)colaPrioridadIndex.get(i-1);
+            int k = 0;
+            while (k<i) {                    
+            List laux2 = (List)terminosSortedbyLeastIndex.get(k);
             List laux3 = new LinkedList();
-            
-            for (int j = (int)laux.get(0); j < (int)laux.get(1); j++) {
-                if ((int)laux2.get(0)==j) {
-                    laux3.add("term_"+(i-1));
-                    j = (int)laux2.get(1);
-                }else{
-                    laux3.add(tokens.get(j));
+            if((int)laux2.get(0)>=(int)laux.get(0) && (int)laux2.get(0)<=(int)laux.get(1)){
+                for (int j = (int)laux.get(0); j < (int)laux.get(1); j++) {
+                    if ((int)laux2.get(0)==j) {
+                        laux3.add("par_"+colaPrioridadIndex.indexOf(laux2));
+                        //add("term_"+(k));
+                        j = (int)laux2.get(1);
+                        laux2 = (List)terminosSortedbyLeastIndex.get(k);
+                        k++;
+                    }else{
+                         laux3.add(tokens.get(j));                      
+                        //laux3.add(laux3.get(j));
+                    }
                 }
+                terminos.add(laux3);
             }
-            terminos.add(laux3);
             //colaPrioridad.add(laux3);
+            k++;
+           
+            }
         }
         /*for (int j = 0; j < 10; j++) {
             int indexSubList = findArray(tokens, (List)terminos.get(terminos.size()-1));
@@ -160,6 +177,89 @@ public class Reconocimiento {
         }
         */
     }
+    
+    public void OrdenOperaciones(){
+        for (int i = 0; i < colaPrioridadIndex.size(); i++) {
+            List laux = (List)colaPrioridadIndex.get(i);
+            terminos.add(tokens.subList((int)laux.get(0)+1, (int)laux.get(1)));
+        }
+        List terminosSortedbyLeastIndex = new LinkedList();
+        for (Object object : colaPrioridadIndex) {
+            List ls = (List)object;
+            terminosSortedbyLeastIndex.add(ls);
+        }
+        
+        SortByLeastIndex(terminosSortedbyLeastIndex, colaPrioridadIndex.size());
+        List teraux = new LinkedList();
+            
+        for (Object object : terminos) {
+            List ls = (List)object;
+            teraux.add(ls);
+        }
+        for (int i = 0; i < colaPrioridadIndex.size(); i++) {
+            
+            List laux = (List) colaPrioridadIndex.get(i);
+            List teraux2 = (List) teraux.get(i);
+            boolean sw = false;
+            
+           
+            for (int j = 0; j < i; j++) {
+                List laux2 = (List)colaPrioridadIndex.get(j);
+                List listBuilder = new LinkedList();
+                
+                if((int)laux2.get(0)>(int)laux.get(0) && (int)laux2.get(0)<(int)laux.get(1)){
+                    for (int k = 0; k < teraux2.size(); k++) {
+                        if(k == ((int)laux2.get(0)-((int)laux.get(0)+1))){
+                            listBuilder.add("parent_"+j);
+                            sw = true;
+                        
+                        }else if(k >((int)laux2.get(0)-((int)laux.get(0)+1)) && k <=((int)laux2.get(1)-((int)laux.get(0)+1))){
+                            listBuilder.add("");
+                        }
+                        else{
+                            listBuilder.add(teraux2.get(k));
+                        }
+                    }
+                    teraux2 = listBuilder;
+                    
+                }
+            }
+            teraux.set(i, teraux2);
+        } 
+        terminos = teraux;
+    }
+    /* Function to sort an array using insertion sort*/
+    void SortByLeastIndex(final List ls, int n)
+    {
+    int i, j;
+    List key;
+    List lx = null;
+    for (i = 1; i < n; i++)
+       {
+       key = (List)ls.get(i);
+       j = i-1;
+ 
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       lx = (List)ls.get(j);
+       while (j >= 0 && (int)lx.get(0) < (int)key.get(0))
+       {
+           ls.set(j+1, lx);
+           j = j-1;
+           try{
+               lx = (List)ls.get(j);
+           }catch(Exception e){
+               
+           }
+           
+       }
+       ls.set(j+1, key);
+        
+       }
+    }
+    
+    
     void buildSintaxTree(){
         for (int i = indexParIzq.size()-1; i >= 0; i--) {
             terminos.add(tokens.subList((int)indexParIzq.get(i)+1, (int)indexParDer.get(i)));
